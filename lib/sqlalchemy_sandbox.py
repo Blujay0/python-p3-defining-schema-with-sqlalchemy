71c93c46176c83b9337be7abf3ae86272f1e09d2
Jay Lim <jlimoman@gmail.com>
2023-06-13 11:50:47 -0600

    complete python-p3-defining-schema-with-sqlalchemy lab

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 
+# create_engine needs to be imported to persist the schema
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Student(Base):
-    pass
+    '''
+    The __tablename__ attribute will eventually be used as the name of our 
+    SQL database table
+    '''
+    __tablename__ = 'students'
 
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    
 if __name__ == '__main__':
-    pass
+    '''
+    the create_all() command tells the engine that any models that were
+    created using Base as a parnet should be used to create tables    
+    '''
+    engine = create_engine('sqlite:///studnets.db')
+    Base.metadata.create_all(engine)
+    
+'''
+run chmod +x lib/sqlalchemy_sandbox.py to make the script executable
+then, run lib/sqlalchemy_sandbox.py from the shell and you should see
+a databse file pop up with a table
+'''
